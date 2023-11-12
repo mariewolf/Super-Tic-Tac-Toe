9df05b83eb8f24aa4e21acc355fcfb9b112dcea0
Marie <marie.wolfel@email.ucr.edu>
2023-11-11 21:43:53 -0800

    fixed little grids

--- STTT/Assets/Scripts/BoardGenerator.cs
+++ STTT/Assets/Scripts/BoardGenerator.cs
@@ -27,14 +27,14 @@ public class BoardGenerator : MonoBehaviour
             for(int j = 0; j < 3; j++)
             {
                 int z = j * 4;
-                
-                GameObject subGrid = Instantiate(subGridPrefab, new Vector3(x, 0, z), Quaternion.identity);
+                GameObject subGrid = Instantiate(subGridPrefab, new Vector3((x + 2.85f*(i-1)), 0, (z + 2.85f*(j-1))), Quaternion.identity);
                 subGrid.transform.parent = transform;
                 for(int s = 0; s < 3; s++)
                 {
                     for(int t = 0; t < 3; t++)
                     {
                         GameObject newSpace = Instantiate(space,new Vector3(x + s + 0.5f, 0, z + t + 0.5f), Quaternion.identity,  subGrid.transform);
+                        
                     }
                 }
                 
